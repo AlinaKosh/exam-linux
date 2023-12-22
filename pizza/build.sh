d14c9159fcaddb7307c0b86bd3d9827387f27d6d
Кошелева Алина <99735329+AlinaKosh@users.noreply.github.com>
2023-12-22 09:02:50 +0400

    Update build.sh

--- pizza/build.sh
+++ pizza/build.sh
@@ -8,5 +8,5 @@ sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin dock
 sudo systemctl enable docker 
 sudo systemctl start docker
 
-sudo firewall-cmd --permanent --add-port=3434/tcp 
+sudo firewall-cmd --permanent --add-port=3000/tcp 
 sudo firewall-cmd --reload
